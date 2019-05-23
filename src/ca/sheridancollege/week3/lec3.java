d600b18731874a4555111e7b82cf32903c75a603
gillsukhjit380 <gillsukhjitkaur1@gmail.com>
2019-05-23 12:56:49 -0400

    manisha the great

---
+++ src/ca/sheridancollege/week3/lec3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3;
+
+/**
+ *
+ * @author sukhjit
+ */
+public class lec3 {
+    private int m;
+
+    /**
+     * @return the m
+     */
+    public int getM() {
+        return m;
+    }
+
+    /**
+     * @param m the m to set
+     */
+    public void setM(int m) {
+        this.m = m;
+    }
+}
