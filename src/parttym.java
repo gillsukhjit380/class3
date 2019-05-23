d1fe26b399b9722796ea667c6577b1d99672dd93
gillsukhjit380 <gillsukhjitkaur1@gmail.com>
2019-05-23 13:18:02 -0400

    manisha the great

---
+++ src/parttym.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sukhjit
+ */
+public class parttym {
+    
+    private int
+
+    class;
+
+    /**
+     * Get the value of class
+     *
+     * @return the value of class
+     */
+    public 
+    
+    private int
+
+    class;
+
+    /**
+     * Get the value of class
+     *
+     * @return the value of class
+     */
+    public int getClass() {
+        return class
+
+    ;
+    }
+        return class
+
+    ;
+    }
+
+    /**
+     * Set the value of class
+     *
+     * @param class new value of class
+     */
+    public void setClass(int
+
+    class
+
+    
+        ) {
+   
+        return class
+
+    ;
+    }
+
+    /**
+     * Set the value of class
+     *
+     * @param class new value of class
+     */
+    public void setClass(int
+
+    class
+
+    
+        ) {
+        this.class = class
+;
+    }
+        this.class = class
+;
+    }
+
+}
