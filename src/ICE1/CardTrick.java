d95e3db077050cbaf17a293607f475b5b2d85a74
gillsukhjit380 <gillsukhjitkaur1@gmail.com>
2019-05-23 20:58:55 -0400

    finished adding code for 3 and 4 to CardTrick.java

--- src/ICE1/CardTrick.java
+++ src/ICE1/CardTrick.java
@@ -1,6 +1,6 @@
 
 package ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -16,11 +16,39 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            
             //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13)+1);
+            //c.setSuit(Card.SUITS[insert call to random])
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c;
         }
-        
+        Scanner a=new Scanner(System.in);
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("enter card value and suit");
+        int value=a.nextInt();
+        String suit=a.next();
+       
+        Card cardobject=new Card();
+        cardobject.setValue(value);
+        cardobject.setSuit(suit);
+        
+       boolean got=false;
+       for(int i=0;i<magicHand.length;i++)
+       {
+           if(magicHand[i].getSuit().equalsIgnoreCase(cardobject.getSuit()) && magicHand[i].getValue() == cardobject.getValue()){
+               got = true;
+               break;
+           }
+       }
+       
+       if(got){
+           System.out.println("You won");
+       }
+       else{
+           System.out.println("You lose");
+       }
+        
         // and search magicHand here
         //Then report the result here
     }
